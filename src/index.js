00cd18aeeeb7cf4429b272f550d3afcd45771cae
YanchevskyiSerhii <serhii.yanchevskii@gmail.com>
2021-07-19 18:30:08 +0300

    feat: add html template

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+const app = document.getElementById('app')
+
+app.innerHTML = `
+
+<div class="todos">
+  <div class="todos-header">
+    <h3 class="todos-title">Todo List</h3>
+    <div>
+      <p>You have<span class="todos-count"></span>item</p>
+      <button type="button" class="todos-clear" style="display: none;">
+        Clear Completed
+      </button>
+    </div>
+  </div>
+  <form class="todos-form" name="todos">
+    <input type="text" placeholder="What's next?" name="todo" />
+  </form>
+  <ul class="todos-list"></ul>
+</div>
+
+`
+
+console.log(app);
